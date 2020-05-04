7fd0d5370a8e924039bda3dd1f3b365af0adca75
yogaandme <63828749+yogaandme@users.noreply.github.com>
2020-05-04 22:34:10 +0530

    format and beautify code

--- dots/homepage-template.php
+++ dots/homepage-template.php
@@ -1,97 +1,79 @@
-<?php
-
-// Asutosh Custom Code: 7, Full page is custom code
-
-/*
-* Template Name: Homepage Template
-* Custom template used for home
-* @package   Dots
-* @author    YogaAndMe team
-*/
-
-?>
+<?php   
+   /*
+   * Template Name: Homepage Template
+   * Custom template used for home
+   * @package   Dots
+   * @author    YogaAndMe team
+   */
+   ?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>
-      <?php wp_title(''); ?>
-    </title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <?php wp_head(); ?>
-
-  </head>
-  <body>
-
-  <!-- Start blog display -->
-  <div class="wrapper">
-
-    <!-- Header -->
-      <div class="site_header">
-
-        <div class="site_title">
-          <a href="<?php echo home_url(); ?>">
-            <?php bloginfo('name'); ?>
-          </a>
-        </div>
-        <div class="site_tagline">
-          <a href="<?php echo home_url(); ?>">
-            <?php bloginfo('description'); ?>
-          </a>
-        </div>
-
+   <head>
+      <meta charset="utf-8">
+      <title>
+         <?php wp_title(''); ?>
+      </title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <?php wp_head(); ?>
+   </head>
+   <body>
+      <!-- Start blog display -->
+      <div class="wrapper">
+         <!-- Header -->
+         <div class="site_header">
+            <div class="site_title">
+               <a href="<?php echo home_url(); ?>">
+               <?php bloginfo('name'); ?>
+               </a>
+            </div>
+            <div class="site_tagline">
+               <a href="<?php echo home_url(); ?>">
+               <?php bloginfo('description'); ?>
+               </a>
+            </div>
+         </div>
+         <?php
+            wp_nav_menu(
+               array(
+                 'menu_class'        => "menu_top",
+                 'theme_location' => 'extra-menu',
+                 'container_class' => 'menu_top'
+               )
+             );
+            ?>
+         <!-- Posts Area -->
+         <div class="posts_area">
+            <?php
+               $args = $args = array(
+               'post_type'=> 'post',
+               'orderby'    => 'ID',
+               'post_status' => 'publish',
+               'order'    => 'DESC',
+               'posts_per_page' => -1 // this will retrive all the post that is published
+               );
+               $posts = get_posts($args);
+                  foreach ($posts as $post) : setup_postdata($post); ?>
+            <div class = "home-page-post-title">
+               <p>
+                  <a href="
+                     <?php the_permalink(); ?>">
+                     <?php the_title(); ?> 
+                     <word class = "read_more"> - Read more</word>
+                  </a>
+               </p>
+            </div>
+            <?php  endforeach; ?>
+            <div class="topnav">
+               <?php posts_nav_link(); ?>
+            </div>
+         </div>
+         <!-- Footer -->
+         <footer class="site_footer">
+            <?php wp_nav_menu( array( 'menu_class' => "menu_bottom", 'theme_location' => 'secondary','container_class' => 'menu_bottom' ) ); ?> 
+            <p>© <?php bloginfo('name'); ?></p>
+         </footer>
       </div>
-			<?php
-		 wp_nav_menu(
-      array(
-        'menu_class'        => "menu_top",
-        'theme_location' => 'extra-menu',
-        'container_class' => 'menu_top'
-      )
-    );
-			?>
-    <!-- Posts Area -->
-    <div class="posts_area">
-
-			<?php
-
-	$args = $args = array(
-	'post_type'=> 'post',
-	'orderby'    => 'ID',
-	'post_status' => 'publish',
-	'order'    => 'DESC',
-	'posts_per_page' => -1 // this will retrive all the post that is published
-	);
-	$posts = get_posts($args);
-	   foreach ($posts as $post) : setup_postdata($post); ?>
-			<div class = "home-page-post-title">
-				<p>
-					<a href="
-						<?php the_permalink(); ?>">
-						<?php the_title(); ?> <word class = "read_more"> - Read more</word>
-					</a>
-				</p>
-			</div>
-			<?php  endforeach; ?>
-
-      <div class="topnav">
-        <?php posts_nav_link(); ?>
-      </div>
-
-
-    </div>
-
-    <!-- Footer -->
-    <footer class="site_footer">
-    <?php wp_nav_menu( array( 'menu_class' => "menu_bottom", 'theme_location' => 'secondary','container_class' => 'menu_bottom' ) ); ?> 
-      <p>© <?php bloginfo('name'); ?></p>
-    </footer>
-
-  </div>
-
-  <!-- End blog display -->
-
-<?php wp_footer(); ?>
-</body>
+      <!-- End blog display -->
+      <?php wp_footer(); ?>
+   </body>
 </html>
--- dots/index.php
+++ dots/index.php
@@ -1,96 +1,82 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>
-      <?php wp_title(''); ?>
-    </title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <?php wp_head(); ?>
-
-  </head>
-  <body>
-
-  <!-- Start blog display -->
-  <div class="wrapper">
-
-    <!-- Header -->
-      <div class="site_header">
-
-        <div class="site_title">
-          <a href="<?php echo home_url(); ?>">
-            <?php bloginfo('name'); ?>
-          </a>
-        </div>
-        <div class="site_tagline">
-          <a href="<?php echo home_url(); ?>">
-            <?php bloginfo('description'); ?>
-          </a>
-        </div>
-
+   <head>
+      <meta charset="utf-8">
+      <title>
+         <?php wp_title(''); ?>
+      </title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <?php wp_head(); ?>
+   </head>
+   <body>
+      <!-- Start blog display -->
+      <div class="wrapper">
+         <!-- Header -->
+         <div class="site_header">
+            <div class="site_title">
+               <a href="<?php echo home_url(); ?>">
+               <?php bloginfo('name'); ?>
+               </a>
+            </div>
+            <div class="site_tagline">
+               <a href="<?php echo home_url(); ?>">
+               <?php bloginfo('description'); ?>
+               </a>
+            </div>
+         </div>
+         <?php
+            wp_nav_menu(array(
+                'menu_class' => "menu_top",
+                'theme_location' => 'extra-menu',
+                'container_class' => 'menu_top'
+            ));
+            ?>
+         <!-- Posts Area -->
+         <div class="posts_area">
+            <?php if (have_posts()):
+               while (have_posts()):
+                   the_post(); ?>
+            <article class="post" role="article" itemscope itemtype="http://schema.org/Article">
+               <?php
+                  // check if the post or page has a Featured Image assigned to it.
+                  if (has_post_thumbnail())
+                  {
+                      the_post_thumbnail();
+                  }
+                  ?>
+               <h1 class="title">
+                  <a class href="<?php the_permalink() ?>">
+                  <?php the_title(); ?>
+                  </a>
+               </h1>
+               <div class="post_meta">
+                  <time class="post_date" datetime="<?php the_time('Y-m-d'); ?>" itemprop="datePublished">
+                  <?php the_time(__('F j, Y')); ?>
+                  </time>
+               </div>
+               <div class="postcontent_list" itemprop="articleBody" data-type-cleanup="true">
+                  <?php the_content('Read More &raquo;'); ?>
+               </div>
+            </article>
+            <?php
+               endwhile;
+               endif; ?>
+         </div>
+         <div id = "comments_area">
+            <h3 id = "comments_title">Comments</h3>
+            <?php comments_template(); ?>
+         </div>
+         <!-- Footer -->
+         <footer class="site_footer">
+            <?php wp_nav_menu(array(
+               'menu_class' => "menu_bottom",
+               'theme_location' => 'secondary',
+               'container_class' => 'menu_bottom'
+               )); ?> 
+            <p>© <?php bloginfo('name'); ?></p>
+         </footer>
       </div>
-      <?php
-      wp_nav_menu(
-        array(
-          'menu_class'        => "menu_top",
-          'theme_location' => 'extra-menu',
-          'container_class' => 'menu_top'
-        )
-      );
-      ?>
-    <!-- Posts Area -->
-    <div class="posts_area">
-
-      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-        <article class="post" role="article" itemscope itemtype="http://schema.org/Article">
-          <?php
-          // check if the post or page has a Featured Image assigned to it.
-if ( has_post_thumbnail() ) {
-   the_post_thumbnail();
-}
-          ?>
-          <h1 class="title">
-            <a class href="<?php the_permalink() ?>">
-              <?php the_title(); ?>
-            </a>
-          </h1>
-
-          <div class="post_meta">
-            <time class="post_date" datetime="<?php the_time('Y-m-d'); ?>" itemprop="datePublished">
-              <?php the_time(__('F j, Y')); ?>
-            </time>
-          </div>
-          <div class="postcontent_list" itemprop="articleBody" data-type-cleanup="true">
-
-          <?php the_content('Read More &raquo;'); ?>
-
-          </div>
-        </article>
-
-      <?php endwhile; endif; ?>
-
-  
-
-
-    </div>
-
-    <div id = "comments_area">
-    <h3 id = "comments_title">Comments</h3>  
-    <?php comments_template(); ?>
-    </div>
-
-    <!-- Footer -->
-    <footer class="site_footer">
-    <?php wp_nav_menu( array( 'menu_class' => "menu_bottom", 'theme_location' => 'secondary','container_class' => 'menu_bottom' ) ); ?> 
-      <p>© <?php bloginfo('name'); ?></p>
-    </footer>
-
-  </div>
-
-  <!-- End blog display -->
-
-<?php wp_footer(); ?>
-</body>
+      <!-- End blog display -->
+      <?php wp_footer(); ?>
+   </body>
 </html>
